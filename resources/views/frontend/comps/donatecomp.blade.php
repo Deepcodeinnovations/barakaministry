1fa5bf757307a27ec763437878f00ec04164989a
Deepcode <deepcodeinnovations@gmail.com>
2021-09-02 08:20:17 +0300

    updated

--- resources/views/frontend/comps/donatecomp.blade.php
+++ resources/views/frontend/comps/donatecomp.blade.php
@@ -6,7 +6,7 @@
                                                 <div id="sc_call_to_action_539461718" class="sc_call_to_action sc_call_to_action_accented sc_call_to_action_style_1 sc_call_to_action_align_left margin_top_huge margin_bottom_huge">
                                                     <div class="sc_call_to_action_info">
                                                         <h2 class="sc_call_to_action_title sc_item_title sc_item_title_without_descr">Join us to Help the Children</b></h2>
-                                                        <div class="sc_call_to_action_descr sc_item_descr">We have  taken up an opportunity of empowering our children with life skills, we look at a child that can one day sustain himself from what he can do best, be talent or just a learnt skill. </div>
+                                                        <div class="sc_call_to_action_descr sc_item_descr">We have taken up an opportunity of empowering our children with life skills, we look at a child that can one day sustain himself from what he can do best, be talent or just a learnt skill.</div>
                                                         <div class="sc_call_to_action_buttons sc_item_buttons">
                                                             <div class="sc_call_to_action_button sc_item_button"><a href="{{ route('web.volunteer')}}" class="sc_button sc_button_square sc_button_style_filled3 sc_button_size_large  sc_button_iconed icon-arrow">Volunteer With Us</a></div>
                                                         </div>
