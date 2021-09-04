9234f9b6d24ca6deb5a804ae8a0383034fb61bf3
Deepcode <deepcodeinnovations@gmail.com>
2021-09-04 12:18:47 +0300

    updated

--- resources/views/frontend/services/servicedetails.blade.php
+++ resources/views/frontend/services/servicedetails.blade.php
@@ -7,7 +7,7 @@
             <div class="page_content_wrap page_paddings_yes">
                 <div class="content_wrap wrapper">
                     <div class="row">
-                    <div class="content col-md-9">
+                    <div class="content col-md-8">
                         <div class="post_item post_item_excerpt post_featured_default post_format_standard odd post-470 post type-post status-publish format-standard has-post-thumbnail hentry category-gallery tag-adoption tag-donation">
                         
                  {!! $service->about !!}
@@ -17,7 +17,7 @@
 
                     </div>
                     <!-- </div> class="content"> -->
-                    <div class="sidebar widget_area scheme_original col-md-3" role="complementary">
+                    <div class="sidebar widget_area scheme_original col-md-4" role="complementary">
                 @include('frontend.comps.sidebar')
                     </div>
                     <!-- /.sidebar -->
