484020c90df2058a1d802f0585f917f19fb49528
Deepcode <deepcodeinnovations@gmail.com>
2021-09-04 10:33:01 +0300

    updated

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,33 +4,29 @@
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta name="format-detection" content="telephone=no"><link rel='shortcut icon' href='' type='image/x-icon' />
-    <link rel='dns-prefetch' href='http://fonts.googleapis.com/' />
-    <link rel='dns-prefetch' href='http://s.w.org/' />
+    <meta name="format-detection" content="telephone=no"><link rel='shortcut icon' href="" type='image/x-icon' />
+    <link rel='dns-prefetch' href="http://fonts.googleapis.com/"/>
+    <link rel='dns-prefetch' href="http://s.w.org/" />
     <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700%7COpen+Sans:300,300i,400,400i,600,600i,700,700i,800,800i%7CRaleway:100,200,300,400,500,600,700,800,900&amp;subset=latin-ext" rel="stylesheet">
-    <link property="stylesheet" rel='stylesheet'href="{{ asset('/frontassets/css/font-face/WCManoNegraBta/stylesheet.css')}}" type='text/css' />
-    <link property="stylesheet" rel='stylesheet' href="{{ asset('/frontassets/css/fontello/fontello.css')}}" type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='rs-plugin-settings-css' href='/frontassets/js/vendor/plugins/revslider/public/assets/css/settings.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='essential-grid-plugin-settings-css' href='/frontassets/js/vendor/plugins/essential-grid/public/assets/css/settings.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-main-style-css' href='/frontassets/css/style.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-main-style-inline-css' href='/frontassets/css/global.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-animation-style-css' href='/frontassets/js/vendor/fw/css/core.animation.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-shortcodes-style-css' href='/frontassets/js/vendor/shortcodes/theme.shortcodes.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-plugin.tribe-events-style-css' href='http://charity-is-hope-html.themerex.net/css/plugin.tribe-events.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-plugin.donations-style-css' href='http://charity-is-hope-html.themerex.net/css/plugin.donations.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-responsive-style-css' href="{{ asset('/frontassets/css/responsive.css')}}" type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='mediaelement-css' href='/frontassets/js/vendor/mediaelement/mediaelementplayer.min.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='trx-donations-style-css'  href='http://charity-is-hope-html.themerex.net/js/vendor/plugins/trx_donations/trx_donations.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-messages-style-css' href='http://charity-is-hope-html.themerex.net/js/vendor/fw/js/core.messages/core.messages.css' type='text/css' />
-    <link property="stylesheet" rel='stylesheet' id='charity_is_hope-swiperslider-style-css' href='/frontassets/js/vendor/fw/js/swiper/swiper.css' type='text/css' />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" />
+    <link rel='stylesheet'href="{{ asset('/frontassets/css/font-face/WCManoNegraBta/stylesheet.css')}}" type='text/css' />
+    <link rel='stylesheet' href="{{ asset('/frontassets/css/fontello/fontello.css')}}" type='text/css' />
+    <link rel='stylesheet' id='rs-plugin-settings-css' href="{{ asset('/frontassets/js/vendor/plugins/revslider/public/assets/css/settings.css')}}"  type='text/css'/>
+    <link rel='stylesheet' id='essential-grid-plugin-settings-css' href="{{ asset('/frontassets/js/vendor/plugins/essential-grid/public/assets/css/settings.css')}}" type='text/css' />
+     <link rel='stylesheet'  href="{{ asset('/frontassets/css/fontello/fontello.css')}}" type='text/css' />
+    <link rel='stylesheet'  href="{{ asset('/frontassets/css/style.css')}}" type='text/css' />
+    <link rel='stylesheet'  href="{{ asset('/frontassets/css/global.css')}}" type='text/css' />
+    <link rel='stylesheet'  href="{{ asset('/frontassets/js/vendor/fw/css/core.animation.css')}}" type='text/css' />
+    <link rel='stylesheet' id='charity_is_hope-shortcodes-style-css' href="{{ asset('/frontassets/js/vendor/shortcodes/theme.shortcodes.css')}}" type='text/css' />
+    <link rel='stylesheet' id='charity_is_hope-responsive-style-css' href="{{ asset('/frontassets/css/responsive.css')}}" type='text/css' />
+    <link rel='stylesheet' id='mediaelement-css' href="/frontassets/js/vendor/mediaelement/mediaelementplayer.min.css" type='text/css' />
+    <link rel='stylesheet' id='charity_is_hope-swiperslider-style-css' href="{{ asset('/frontassets/js/vendor/fw/js/swiper/swiper.css')}}" type='text/css' />
     <link href="{{ asset('/adminassets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet"/>
-
+    <link href="{{ asset('/adminassets/css/icons.css')}}" rel="stylesheet"/>
 
 <title>Baraka Ministries | @yield('title') </title>
 </head>
 <body class="home page page-template-default body_style_wide top_style_header_1 body_filled article_style_stretch layout_single-standard template_single-standard scheme_original top_panel_show top_panel_above sidebar_hide sidebar_outer_hide">
-    <a id="toc_home" class="sc_anchor" title="Home" data-description="&lt;i&gt;Return to Home&lt;/i&gt; - &lt;br&gt;navigate to home page of the site" data-icon="icon-home" data-url="http://charity-is-hope.themerex.net/" data-separator="yes"></a>
+    <a id="toc_home" class="sc_anchor" title="Home" data-description="&lt;i&gt;Return to Home&lt;/i&gt; - &lt;br&gt;navigate to home page of the site" data-icon="icon-home" data-url="http://barakaministries.org/" data-separator="yes"></a>
     <a id="toc_top" class="sc_anchor" title="To Top" data-description="&lt;i&gt;Back to top&lt;/i&gt; - &lt;br&gt;scroll to top of the page" data-icon="icon-double-up" data-url="" data-separator="yes"></a>
 
     <div id="pageloader">   
